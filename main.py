2e55603781aa9e0af897494af4da863223ffa790
Ryan Lillah <ryanrlillah@gmail.com>
2023-09-20 14:47:55 +0400

    Learning lists

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+# R. Lillah
+# 10 September 2023
+# Learning Python
+from library import welcome_message
+
+# import this
+
+
 def main():
-    print(__name__)
+    # print(__name__)
+    # book_name, author, release_date, book_rating = 'The Art of Unix Programming', 'Eric S. Raymond', 2003, 4.4
+    # print(f'The book {book_name} was written by {author}, and released in {release_date}. '
+    #       f'It has a rating of {book_rating} on Amazon.com')
+    # print(book_name, author, release_date, book_rating)
+
+    # num = 28
+    # print(f'Your favorite number is {num}')
+    # print('Your favorite number is ' + str(num))
+    #
+    # print(2*4)
+    # print(this)
+    # number = 32
+    # name = 'Apple'
+    # print(type(number))
+    # print(type(name))
+
+    fruits = ['grapes', 'apples', 'pineaplle']
+    print(fruits[-2])
+    print(fruits[1].title())
+
+    scores =[55, 92, 73]
+    print(scores)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     main()
-
-# Hello, World!
